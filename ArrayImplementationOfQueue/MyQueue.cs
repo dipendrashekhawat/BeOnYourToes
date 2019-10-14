5f9af706382c8b0e51bda696f83f8223ebb6dbf5
Dipendra.Shekhawat <dipendra.shekhawat@greenorbit.com>
2019-10-14 17:37:35 +0530

    Added code to display queue elements and added some pre-checks anf validation around queue

--- ArrayImplementationOfQueue/MyQueue.cs
+++ ArrayImplementationOfQueue/MyQueue.cs
@@ -4,15 +4,15 @@ namespace ArrayImplementationOfQueue
 {
     class MyQueue
     {
-        private int []arr = new int [10]; // fixed sized integer array
-        private int front = 0, rear = 0;
+        private static int []arr = new int [5] {1, 2, 3, 4, 5}; // fixed sized integer array
+        private static int front = 0, rear = 0;
 
         public void Enqueue(int num)
         {
             // if array is already full
             if(rear == arr.Length)
             {
-                Console.WriteLine("Queue is full");
+                Console.WriteLine("\nQueue is full while inserting elemenet:  {0}", num);
                 return;
             }
             else
@@ -33,7 +33,7 @@ namespace ArrayImplementationOfQueue
             }
             else
             {
-                for(int i = 0; i<rear-1; i++)
+                for(int i = 0; i< rear-1; i++)
                 {
                     arr[i] = arr[i+1];
                 }
@@ -48,9 +48,27 @@ namespace ArrayImplementationOfQueue
             return;
         }
 
+        public void Display(int size)
+        {   
+            // if array is empty
+            if (front == rear)
+            {
+                Console.WriteLine("No elements");
+                return;
+            }
+
+            for(int i = front; i< rear; i++)  
+            {  
+                Console.Write("{0}  ", arr[i]);  
+            }
+        }
+
         static void Main(string[] args)
         {
             MyQueue queue = new MyQueue();
+
+            Console.Write("\nElements in queue are: ");
+            queue.Display(5);
             
             queue.Enqueue(1);
             queue.Enqueue(2);
@@ -58,8 +76,23 @@ namespace ArrayImplementationOfQueue
             queue.Enqueue(4);
             queue.Enqueue(5);
 
-            queue.Dequeue();
-            queue.Dequeue();
+            Console.Write("\nElements in queue after Enqueing: ");
+            queue.Display(5);
+
+            queue.Enqueue(6); // will get an error here as the array will be full and eement '6' will not be inserted
+            
+            queue.Dequeue(); // 1 will be removed
+            queue.Dequeue(); // 2 will be removed
+
+            Console.Write("\nElements in queue after Dequeing: ");
+            queue.Display(5);
+
+            // 6 & 7 will be inserted as we have removed 2 elements from the array
+            queue.Enqueue(6); 
+            queue.Enqueue(7);
+
+            Console.Write("\nElements in queue after Enqueing again: ");
+            queue.Display(5);
 
         }
     }
